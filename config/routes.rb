85445904f3a6829954071a00a64351bc48d4c22c
Simon Acosta <acosta1204@gmail.com>
2016-11-28 17:23:16 +0100

    Working on iti 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :sandwich_ingredients
-  resources :ingredients
-  resources :sandwiches
+    namespace :api do
+    namespace :v1 do
+      resources :sandwiches do
+        resources :ingredients
+        post 'ingredients/add', to: 'sandwiches#add'
+      end
+    end
+   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
